2186272fdd4dabb5746ad012462954b1a3de6f18
Muhammad Taufik <mailtaufik@gmail.com>
2012-01-11 09:36:04 +0700

    Initial Commit

---
+++ Drawing/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Drawing
+//
+//  Created by Muhammad Taufik on 1/11/12.
+//  Copyright (c) 2012 Beetlebox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Drawing/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Drawing
+//
+//  Created by Muhammad Taufik on 1/11/12.
+//  Copyright (c) 2012 Beetlebox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
